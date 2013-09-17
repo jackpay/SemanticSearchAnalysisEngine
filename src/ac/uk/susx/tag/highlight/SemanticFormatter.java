757c7d72983fc38bcb81948c43d861d688572def
Jack <jp242@sussex.ac.uk>
2013-09-17 16:35:44 +0100

    add highlighter formatter experiments

--- src/ac/uk/susx/tag/highlight/SemanticFormatter.java
+++ src/ac/uk/susx/tag/highlight/SemanticFormatter.java
@@ -6,26 +6,26 @@ import org.apache.lucene.search.highlight.TokenGroup;
 
 public class SemanticFormatter implements Formatter {
 	
-	private final String PRE;
-	private final String POST;
+	private final String pre;
+	private final String post;
 	private static final String START = "<";
 	private static final String END = ">";
 	private static final String DEFAULT_PRE = START + "SEM";
 	private static final String DEFAULT_POST = START + "/SEM" + END;
-	private static final String DELIM = "=";
-	private static final String POS_TAG = "pos" + DELIM;
-	private static final String CHUNK_TAG = "chunk" + DELIM;
-	private static final String PROX_TAG = "prox" + DELIM;
-	private static final String CLOSE_TO = "closeto" + DELIM;
-	private static final String SPACE = " ";
+	private static final String POS_TAG = "pos";
+	private static final String CHUNK_TAG = "chunk";
+	private static final String PROX_TAG = "prox";
+	private static final String CLOSE_TO = "closeto";
+	private static final String ATT_DELIM = "=";
+	private static final String SPACE_DELIM = " ";
 	
 	public SemanticFormatter(){
 		this(DEFAULT_PRE,DEFAULT_POST);
 	}
 	
 	public SemanticFormatter(String pre, String post){
-		this.PRE = pre;
-		this.POST = post;
+		this.pre = pre;
+		this.post = post;
 	}
 
 	@Override
@@ -41,8 +41,8 @@ public class SemanticFormatter implements Formatter {
 	    	System.err.println(token.getPositionLength());
 	    	System.err.println(token.getFlags());
 	    	System.err.println(token.getBytesRef());
-	    	//System.err.println(originalText);
-	    	out = PRE + SPACE + POS_TAG + token.getPayload() + END + originalText + POST;
+	    	System.err.println(originalText);
+	    	out = pre + SPACE_DELIM + POS_TAG + ATT_DELIM + token.getPayload() + END + originalText + post;
 	    }
 		return out;
 	}
