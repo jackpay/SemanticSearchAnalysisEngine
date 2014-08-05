de5acb912bbc05be875b205a601971f039ee40c2
Jack <jp242@sussex.ac.uk>
2014-08-05 16:48:50 +0100

    added custom update processor

---
+++ src/ac/uk/susx/tag/update/PartialUpdateDocumentProcessorFactory.java
@@ -0,0 +1,63 @@
+package ac.uk.susx.tag.update;
+
+import java.io.IOException;
+import java.util.HashSet;
+
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrInputDocument;
+import org.apache.solr.request.SolrQueryRequest;
+import org.apache.solr.response.SolrQueryResponse;
+import org.apache.solr.update.AddUpdateCommand;
+import org.apache.solr.update.processor.UpdateRequestProcessor;
+import org.apache.solr.update.processor.UpdateRequestProcessorFactory;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.response.QueryResponse;
+
+// Retrieves the document from the index and creates a new document with the old fields and updated new fields.
+public class PartialUpdateDocumentProcessorFactory extends UpdateRequestProcessorFactory
+{
+  @Override
+  public UpdateRequestProcessor getInstance(SolrQueryRequest req, SolrQueryResponse rsp, UpdateRequestProcessor next)
+  {
+    return new PartialUpdateDocumentProcessor(next);
+  }
+  
+  public class PartialUpdateDocumentProcessor extends UpdateRequestProcessor {
+	  
+	  public static final String solrURL = "http://localhost:8983/solr";
+	  public final HashSet<String> allowedFields = new HashSet<String>() {}; // Used as a security measure to prevent unwanted changes.
+	  
+	  public PartialUpdateDocumentProcessor( UpdateRequestProcessor next) {
+		  super( next );
+	  }
+
+	  @Override
+	  public void processAdd(AddUpdateCommand cmd) throws IOException {
+		  
+	      SolrInputDocument doc = cmd.getSolrInputDocument();
+	      String id = (String) doc.getFieldValue("id");
+		  HttpSolrServer solr = new HttpSolrServer(solrURL); // Get the document that requires updating.
+		  
+		  SolrQuery query = new SolrQuery();
+		  query.setQuery("id:" + "\"" + id + "\"");
+		  query.setStart(0);								// Query the id to return ONLY the one document which requires updating.
+		  try {
+			QueryResponse response = solr.query(query);
+			SolrDocument result = response.getResults().get(0);
+			for(String fieldName : result.getFieldNames()) {
+				if(doc.getFieldValue(fieldName) == null || doc.getFieldValue(fieldName) == ""){
+					doc.setField(fieldName, result.getFieldValue(fieldName));		// Add all fields from original document that are not in the new document.
+				}
+			}
+		} catch (SolrServerException e) {
+			e.printStackTrace();
+		}
+
+		// pass it up the chain
+    	super.processAdd(cmd);
+    }
+  }
+  
+}
